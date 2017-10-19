88cf4f57aff3d7ebf9d35f9f1e167a1bb5d83eb2
kthangtd <kthangtd@lecle.co.kr>
2017-10-19 13:36:24 +0700

    update new code

---
+++ Gift-Display-iOS/MainViewController.m
@@ -0,0 +1,39 @@
+//
+//  MainViewController.m
+//  Gift-Display-iOS
+//
+//  Created by Ngo Than Phong on 3/10/17.
+//  Copyright © 2017 kthangtd. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "GiftDisplayArea.h"
+#import "GiftEvent.h"
+
+@interface MainViewController ()
+
+@property (weak, nonatomic) IBOutlet GiftDisplayArea * giftDisplayArea;
+
+@end
+
+@implementation MainViewController
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark ---- < Button Action >
+
+- (IBAction)buttonClicked:(UIButton *)sender {
+    NSString * text = sender.titleLabel.text;
+    NSArray * arr = [text componentsSeparatedByString:@"-"];
+    GiftEvent * event = [[GiftEvent alloc] init];
+    event.senderName = [arr objectAtIndex:0];
+    event.senderId = [event.senderName isEqualToString:@"A"] ? 1 : 2;
+    event.giftId = [[arr objectAtIndex:1] integerValue];
+    event.giftCount = [[arr objectAtIndex:2] integerValue];
+    [self.giftDisplayArea pushGiftEvent:event];
+}
+
+@end
---
+++ Gift-Display-iOS/model/GiftEvent.m
@@ -0,0 +1,24 @@
+//
+//  GiftEvent.m
+//  Gift-Display-iOS
+//
+//  Created by Ngo Than Phong on 3/10/17.
+//  Copyright © 2017 kthangtd. All rights reserved.
+//
+
+#import "GiftEvent.h"
+
+@implementation GiftEvent
+
+- (void)dealloc {
+    self.senderName = nil;
+}
+
+- (BOOL)isEqual:(id)object {
+    if (object == nil) {
+        return NO;
+    }
+    return self.senderId == ((GiftEvent *)object).senderId && self.giftId == ((GiftEvent *)object).giftId;
+}
+
+@end
